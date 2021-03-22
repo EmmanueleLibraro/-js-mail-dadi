c8946871ba8fd4101ecacb204d39e7b298b4c575
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-22 16:15:17 +0100

    email

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('ciao')
+//Chiedi all’utente la sua email, stampa un messaggio appropriato sull’esito del controllo.
+
+var indirizzoEmail = ['pollino@live.it', 'lollino@live.it', 'picco@live.it'];
+console.log(indirizzoEmail);
+console.log("Numero elementi array", indirizzoEmail.length);
+
+var indirizzo = prompt("Inserisci email");
