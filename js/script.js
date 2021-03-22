3289667914b9266be7aec2c763818c8cbb520a32
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-22 17:26:25 +0100

    email

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 //Chiedi all’utente la sua email, stampa un messaggio appropriato sull’esito del controllo.
 
+
+//EMAIL
 var indirizzoEmail = ['pollino@live.it', 'lollino@live.it', 'picco@live.it'];
 console.log(indirizzoEmail);
 console.log("Numero elementi array", indirizzoEmail.length);
 
-var indirizzo = prompt("Inserisci email" );
+var indirizzo = prompt("Inserisci email");
 
- 
-if ( indirizzoEmail === indirizzoEmail){
-    console.log("Utente trovato", indirizzoEmail);
-} else{
-    console.log("email non valida");
+var trovataEmailValida = false;
+
+for (var i = 0; i < indirizzoEmail.length; i++){
+    if (indirizzo === indirizzoEmail[i]){
+        trovataEmailValida = true;
+    } 
 }
+
+if (trovataEmailValida){
+    console.log("Email trovata");
+} else{
+    console.log("Email non valida");
+}
+
+
+//DADO
+// var numero = ['1', '2', '3', '4', '5', '6'];
+
+// console.log(numero);
+
+// for (var i = 0; i < numero.length; i++) {
+
+// }
+
+// var giocatore = parseInt( prompt("Inserisci un numero da 1 a 6") );
